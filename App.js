69a066d8023072e27758f577808f74757dcd7148
rhelal <raed.helal@maiia.com>
2021-04-01 12:02:16 +0200

    add navigation

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import PageStatusCommand from './src/pages/PageStatusCommand.js';
+import Router from './src/navigation';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <PageStatusCommand />
-    </View>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+
   );
 }
 
